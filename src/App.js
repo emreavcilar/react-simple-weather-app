60c9140c402794acd9d6340a8cc6ed490a54f72b
Emre Avcılar <emreavcilar@gmail.com>
2021-08-18 22:55:39 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [temp, setTemp] = useState(null);
+  const [coords, setCoords] = useState();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords }) => setCoords(coords));
+  }, []);
+
+  useEffect(() => {
+    if (!coords) {
+      return;
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`)
+      .then(res => res.json())
+      .then(data => setTemp(data?.main?.temp));
+  }, [coords])
+
   return (
     <div className="App">
-      test
+      <div>Weather</div>
+      <div>{temp === null ? 'n/a' : temp}&deg;</div>
     </div>
   );
 }
